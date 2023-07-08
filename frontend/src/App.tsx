5b4594ee904705e4c03767bdad2ed4026bf4816c
queens kisivuli <queenskisivuli@gmail.com>
2023-07-08 08:38:52 +0300

    ui routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
-import './App.css'
+import "./App.css";
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+import SignIn from "./pages/Login/SignIn";
+import Home from "./pages/Home/Home";
+import SignUp from "./pages/Register/SignUp";
 
 function App() {
-
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<Home />} />
+          <Route path="/contact" element={<Home />} />
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/register" element={<SignUp />} />
+          <Route path="/dashboard" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
