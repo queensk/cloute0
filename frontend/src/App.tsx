309059927f52f8fd3dec0b9791954724c80d8d74
queens kisivuli <queenskisivuli@gmail.com>
2023-07-10 12:01:24 +0300

    add ui switch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,57 @@
 import "./App.css";
-import { Routes, BrowserRouter, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import SignIn from "./pages/Login/SignIn";
 import Home from "./pages/Home/Home";
 import SignUp from "./pages/Register/SignUp";
+import Notification from "./pages/Notification/Notification";
+import Messages from "./pages/Messages/Messages";
+import Friends from "./pages/Friends/Friends";
+import Profile from "./pages/Profile/Profile";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "./Redux/store";
 
 function App() {
+  const ui = useSelector((state: RootState) => state.ui.ui);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (ui) {
+      const uiValue = ui;
+      switch (uiValue) {
+        case "Home":
+          navigate("/", { replace: true });
+          break;
+        case "Notification":
+          navigate("/notification", { replace: true });
+          break;
+        case "Messages":
+          navigate("/messages", { replace: true });
+          break;
+        case "Friends":
+          navigate("/friends", { replace: true });
+          break;
+        case "Profile":
+          navigate("/profile", { replace: true });
+          break;
+        default:
+          break;
+      }
+    }
+  }, [ui]);
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<Home />} />
-          <Route path="/contact" element={<Home />} />
-          <Route path="/login" element={<SignIn />} />
-          <Route path="/register" element={<SignUp />} />
-          <Route path="/dashboard" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/notification" element={<Notification />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/friends" element={<Friends />} />
+        <Route path="/profile" element={<Profile />} />/
+        <Route path="/about" element={<Home />} />
+        <Route path="/contact" element={<Home />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<SignUp />} />
+        <Route path="/dashboard" element={<Home />} />
+      </Routes>
     </>
   );
 }
