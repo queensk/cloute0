f49eba27e4c24335967386ea0ab5d883de24ef9a
queens kisivuli <queenskisivuli@gmail.com>
2023-07-10 11:09:06 +0300

    add react persists

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,14 +4,16 @@ import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-
 import { store, persistor } from "./Redux/store.ts";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
