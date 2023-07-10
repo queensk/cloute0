de2202f694b398e612085df41edad2ea82fca4dc
queens kisivuli <queenskisivuli@gmail.com>
2023-07-10 12:03:06 +0300

    friends comoponent

---
+++ frontend/src/componets/FriendData/FriendData.tsx
@@ -0,0 +1,13 @@
+import "./FriendData.css";
+
+export default function FriendData() {
+  return (
+    <div className="friend-data">
+      <img src="https://picsum.photos/200/300" alt="" />
+      <div className="friend-data-info">
+        <p>Friend Name</p>
+        <p>Friend Email</p>
+      </div>
+    </div>
+  );
+}
