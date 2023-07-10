cdb0fcac5b9f1e18cdeb34450b1242ffbc7542bd
queens kisivuli <queenskisivuli@gmail.com>
2023-07-10 12:14:23 +0300

    main ui interface

---
+++ frontend/src/componets/Main/Main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Main.css";
+import Post from "../Post/Post";
+
+export default function Main() {
+  return (
+    <div className="main-container">
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+}
