42e8772526975a5c6496e48418c1d3ed4284693a
queens kisivuli <queenskisivuli@gmail.com>
2023-07-14 17:01:06 +0300

    user types

---
+++ frontend/src/types/user.tsx
@@ -0,0 +1,13 @@
+export type UserData = {
+  id: string;
+  name: string;
+  userName: string;
+  email: string;
+  profilePic: string | null;
+  bio: string;
+  location: string;
+  website: string;
+  joined: string;
+  createAt: string;
+  updatedAt: string;
+};
