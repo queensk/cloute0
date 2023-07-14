be0f09ad5c657dc2ba9938536f223b7b2c49ee4b
queens kisivuli <queenskisivuli@gmail.com>
2023-07-14 16:58:52 +0300

    signup user page

--- frontend/src/pages/Register/SignUp.tsx
+++ frontend/src/pages/Register/SignUp.tsx
@@ -1,4 +1,3 @@
-// SignUp.tsx
 import React from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -6,6 +5,8 @@ import * as yup from "yup";
 import "./Register.css";
 import Logo from "./Frame 7.png";
 import { Link } from "react-router-dom";
+import { useRegisterMutation } from "../../features/auth/authApi";
+import { useNavigate } from "react-router-dom";
 
 const schema = yup.object().shape({
   name: yup.string().required("Name is required"),
@@ -17,15 +18,19 @@ const schema = yup.object().shape({
     .string()
     .min(8, "Password must be at least 8 characters")
     .required("Password is required"),
+  userName: yup.string().required("User Name is required"),
 });
 
 type FormData = {
   name: string;
+  userName: string;
   email: string;
   password: string;
 };
 
 const SignUp: React.FC = () => {
+  const [registerMutation] = useRegisterMutation();
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
@@ -33,42 +38,58 @@ const SignUp: React.FC = () => {
   } = useForm<FormData>({
     resolver: yupResolver(schema),
   });
+
   const onSubmit = async (data: FormData) => {
     console.log(data);
+    try {
+      registerMutation(data);
+      navigate("/", { replace: true });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    // add class name to the signUp page
     <div className="signUp-container">
       <div className="signUp-form-container">
         <form onSubmit={handleSubmit(onSubmit)}>
           <h3>Create your account</h3>
           <div>
-            <label htmlFor="name"></label>
+            <label htmlFor="name">Name</label>
             <input
               id="name"
-              placeholder="Name"
               type="text"
+              placeholder="Name"
               {...register("name")}
             />
             {errors.name && <span>{errors.name.message}</span>}
           </div>
           <div>
-            <label htmlFor="email"></label>
+            <label htmlFor="userName">Username</label>
+            <input
+              id="userName"
+              type="text"
+              placeholder="Username"
+              {...register("userName")}
+            />
+            {errors.userName && <span>{errors.userName.message}</span>}
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
             <input
               id="email"
-              placeholder="Email"
               type="email"
+              placeholder="Email"
               {...register("email")}
             />
             {errors.email && <span>{errors.email.message}</span>}
           </div>
           <div>
-            <label htmlFor="password"></label>
+            <label htmlFor="password">Password</label>
             <input
               id="password"
-              placeholder="Password"
               type="password"
+              placeholder="Password"
               {...register("password")}
             />
             {errors.password && <span>{errors.password.message}</span>}
@@ -78,7 +99,7 @@ const SignUp: React.FC = () => {
           </button>
           <div className="signUp-terms-container">
             <p>
-              By signing up you agree to our Terms of Service and Privacy
+              By signing up, you agree to our Terms of Service and Privacy
               Policy, and confirm that you are at least 18 years old.
             </p>
             <p>
