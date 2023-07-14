b1235ad2ac80f9f2f46a34c825b2d9e72ab94468
queens kisivuli <queenskisivuli@gmail.com>
2023-07-14 16:56:41 +0300

    post input data

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -3,16 +3,19 @@ import SideNav from "../../componets/SideNav/SideNav";
 import "./Home.css";
 import Main from "../../componets/Main/Main";
 import Suggestions from "../../componets/Suggestions/Suggestions";
+import { ContextProvider } from "../../Context/inputContext";
 
 export default function Home() {
   return (
-    <div className="home-container">
-      <Nav />
-      <div className="home-main">
-        <SideNav />
-        <Main />
-        <Suggestions />
+    <ContextProvider>
+      <div className="home-container">
+        <Nav />
+        <div className="home-main">
+          <SideNav />
+          <Main />
+          <Suggestions />
+        </div>
       </div>
-    </div>
+    </ContextProvider>
   );
 }
