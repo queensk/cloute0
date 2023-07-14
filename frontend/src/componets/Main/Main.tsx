d90e9bebf6568879d0376d02a31d6136e403ff17
queens kisivuli <queenskisivuli@gmail.com>
2023-07-14 18:34:05 +0300

    add input to main

--- frontend/src/componets/Main/Main.tsx
+++ frontend/src/componets/Main/Main.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import { useContext } from "react";
 import "./Main.css";
 import Post from "../Post/Post";
+import { Context } from "../../Context/inputContext";
+import PostInput from "../PostInput/PostInput";
 
 export default function Main() {
+  const { showPostInput } = useContext(Context);
+
   return (
     <div className="main-container">
+      {showPostInput && <PostInput />}
       <Post />
       <Post />
       <Post />
