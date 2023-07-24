adeb928ed724c90bee37b665cdc29dc5ee87dc16
queens kisivuli <queenskisivuli@gmail.com>
2023-07-24 14:47:16 +0300

    socket ping

--- Backend/index.js
+++ Backend/index.js
@@ -8,13 +8,26 @@ import cors from "cors";
 import routes from "./routes/routes.js";
 import { config } from "./utils/db.js";
 import userAuthorized from "./utils/authMiddleware.js";
+import http from "http";
+import { Server } from "socket.io";
+import chartMessage from "./controllers/chartMessageController.js";
+import notificationHandler from "./controllers/notificationController.js";
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  pingInterval: 60000,
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
 
 app.use(userAuthorized);
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+chartMessage(io);
+notificationHandler(io);
 
 /**
  * A default handler that sends a greeting message.
@@ -28,6 +41,6 @@ app.get("/", (req, res) => {
 /**
  * Starts the server on port 8085 and logs a message.
  */
-app.listen(config.port | 8085, () => {
+server.listen(config.port | 8085, () => {
   console.log(`Server is running on ${config.url}`);
 });
