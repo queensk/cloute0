56dbe98fc48f06a50c045ada7ffd5b0cf85486ee
queens kisivuli <queenskisivuli@gmail.com>
2023-07-24 12:50:08 +0300

    message user

--- frontend/src/pages/Messages/Messages.tsx
+++ frontend/src/pages/Messages/Messages.tsx
@@ -2,7 +2,7 @@ import Nav from "../../componets/Nav/Nav";
 import SideNav from "../../componets/SideNav/SideNav";
 import "./Messages.css";
 import MainMessages from "../../componets/MainMessages/MainMessages";
-import ChatUsers from "../../componets/ChartUsers/ChatUsers";
+import ChatUsers from "../../componets/ChatUsers/ChatUsers";
 
 export default function Messages() {
   return (
