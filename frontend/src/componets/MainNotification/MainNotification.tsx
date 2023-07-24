0dc5bd7138917cfca1ea0e7761bf3f26cdbc4764
queens kisivuli <queenskisivuli@gmail.com>
2023-07-24 12:28:14 +0300

    main user data notifications

--- frontend/src/componets/MainNotification/MainNotification.tsx
+++ frontend/src/componets/MainNotification/MainNotification.tsx
@@ -1,6 +1,36 @@
+import React, { useState, useEffect } from "react";
 import "./MainNotification.css";
 import Logo from "./Frame 7.png";
+import socketIOClient from "socket.io-client";
+
 export default function MainNotification() {
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    const ENDPOINT = "http://localhost:8085"; // Replace with your server's endpoint
+
+    // Establish a WebSocket connection with the server
+    const socket = socketIOClient(ENDPOINT, {
+      query: {
+        receiverId: "YOUR_RECEIVER_ID", // Replace with the receiver's ID (e.g., user ID)
+      },
+    });
+
+    // Listen for incoming notifications from the server
+    socket.on("notification", (notification) => {
+      console.log("New notification received:", notification);
+      setNotifications((prevNotifications) => [
+        notification,
+        ...prevNotifications,
+      ]);
+    });
+
+    // Clean up the WebSocket connection when the component unmounts
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <div className="main-notification-container">
       <div className="main-notification-header">
@@ -11,12 +41,13 @@ export default function MainNotification() {
           <img src={Logo} alt="" />
         </div>
         <div className="main-notification-content">
-          <p>queens</p>
-          <p>
-            We would like to inform you that your account has been successfully
-            created. You can now log in using your credentials and acced
-            .....more
-          </p>
+          {/* Map through the notifications and render each one */}
+          {notifications.map((notification, index) => (
+            <div key={index}>
+              <p>{notification.senderId}</p>
+              <p>{notification.message}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
