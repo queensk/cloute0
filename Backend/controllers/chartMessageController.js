3fd068500e1a8bea0954d6ef4999ee55726de754
queens kisivuli <queenskisivuli@gmail.com>
2023-07-26 09:14:49 +0300

    chart socket update

--- Backend/controllers/chartMessageController.js
+++ Backend/controllers/chartMessageController.js
@@ -5,14 +5,33 @@ import { sqlConfig } from "../utils/db.js";
 import mssql from "mssql";
 import { v4 as uuidv4 } from "uuid";
 
-const chartMessage = (io) => {
+const chartMessage = async (io) => {
+  let numberOfConnections = 0;
   io.on("connection", (socket) => {
     console.log("connected");
+    numberOfConnections++;
+    console.log(numberOfConnections);
     const roomId = socket.handshake.query.roomId;
     socket.join(roomId);
 
     socket.on("typing", async (data) => {
+      // You need to access data.roomId instead of data.roomId
       socket.to(data.roomId).emit("typing", data);
+    });
+
+    socket.on("joinRoom", async (room) => {
+      console.log(`join room ${room.roomId}`);
+      socket.join(roomId);
+      let userId = room.userId;
+      let onlineUsers = {};
+      console.log(onlineUsers);
+      if (onlineUsers[userId]) {
+        onlineUsers[userId].push(socket.id);
+      } else {
+        onlineUsers[userId] = [socket.id];
+      }
+      console.log(onlineUsers);
+      socket.to(roomId).emit("onlineUsers", onlineUsers);
     });
 
     socket.on("room", async (data) => {
@@ -33,8 +52,6 @@ const chartMessage = (io) => {
                 WHERE (um.senderId = @senderId AND um.receiverId = @receiverId) OR (um.senderId = @receiverId AND um.receiverId = @senderId) AND um.roomId = @roomId
                 ORDER BY um.createAt ASC;`
           );
-        // console.log("room data");
-        // console.log(result.recordset);
 
         socket.emit("roomMessages", result.recordset);
       } catch (err) {
@@ -77,7 +94,7 @@ const chartMessage = (io) => {
               "SELECT * FROM socialClout.user_messages WHERE roomId = @roomId AND senderId = @senderId AND receiverId = @receiverId ORDER BY createAt DESC"
             );
           const newChat = selectResult.recordset[0];
-          socket.to(roomId).emit("chartMessage", newChat);
+          io.to(roomId).emit("chartMessage", newChat);
         }
       } catch (err) {
         console.error(err);
@@ -86,10 +103,33 @@ const chartMessage = (io) => {
       }
     });
 
+    // const receiverId = socket.handshake.query.receiverId;
+    // socket.join(receiverId);
+
+    // socket.on("sendNotification", (notification) => {
+    //   console.log(notification);
+    //   const receiverSocketId = notification.receiverId;
+    //   io.to(receiverSocketId).emit("notification", notification);
+    //   // No need to disconnect the socket after sending a notification
+    // });
+
     socket.on("disconnect", () => {
-      console.log("user disconnected");
-      socket.leave(roomId);
+      console.log("Disconnected");
+      numberOfConnections--;
+      console.log(numberOfConnections);
+      // for (let user in onlineUsers) {
+      //   let index = onlineUsers[user].indexOf(socket.id);
+      //   if (index !== -1) {
+      //     onlineUsers[user].splice(index, 1);
+      //     if (onlineUsers[user].length === 0) {
+      //       delete onlineUsers[user];
+      //     }
+      //     socket.to(roomId).emit("onlineUsers", onlineUsers);
+      //     break;
+      //   }
+      // }
     });
   });
 };
+
 export default chartMessage;
