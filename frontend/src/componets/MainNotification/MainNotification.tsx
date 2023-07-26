13a32dcb467a3d4f675c91472711143441b0dec0
queens kisivuli <queenskisivuli@gmail.com>
2023-07-26 09:23:37 +0300

    user main notification

--- frontend/src/componets/MainNotification/MainNotification.tsx
+++ frontend/src/componets/MainNotification/MainNotification.tsx
@@ -1,35 +1,65 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./MainNotification.css";
 import Logo from "./Frame 7.png";
-import socketIOClient from "socket.io-client";
+import { RootState } from "../../Redux/store";
+import { useSelector } from "react-redux";
+import { NotificationType } from "../../types/Notification";
+import moment from "moment";
+
+interface Notification {
+  id: string;
+  senderId: string;
+  receiverId: string;
+  message: string;
+  seen: boolean;
+  type: NotificationType;
+  createdAt: string;
+  updatedAt: string;
+}
 
 export default function MainNotification() {
-  const [notifications, setNotifications] = useState([]);
+  const [notifications, setNotifications] = useState<Notification[]>([]);
+  const userId = useSelector((state: RootState) => state.auth.user.id);
+  const socket = useSelector((state: RootState) => state.socket.socket);
 
   useEffect(() => {
-    const ENDPOINT = "http://localhost:8085"; // Replace with your server's endpoint
+    if (socket) {
+      socket.emit("getNotifications", userId);
 
-    // Establish a WebSocket connection with the server
-    const socket = socketIOClient(ENDPOINT, {
-      query: {
-        receiverId: "YOUR_RECEIVER_ID", // Replace with the receiver's ID (e.g., user ID)
-      },
+      socket.on("notification", (notifications: Notification[]) => {
+        setNotifications((prevNotifications) => [
+          ...notifications,
+          ...prevNotifications,
+        ]);
+      });
+    }
+  }, [socket, userId]);
+
+  const handleNotificationClick = (index: number) => {
+    setNotifications((prevNotifications) => {
+      const updatedNotifications = [...prevNotifications];
+      updatedNotifications[index].seen = true;
+      return updatedNotifications;
     });
+  };
 
-    // Listen for incoming notifications from the server
-    socket.on("notification", (notification) => {
-      console.log("New notification received:", notification);
-      setNotifications((prevNotifications) => [
-        notification,
-        ...prevNotifications,
-      ]);
-    });
+  const getNotificationStyle = (type: NotificationType, read: boolean) => {
+    let baseStyle = "main-notification-item";
+    if (read) {
+      baseStyle += " read";
+    }
 
-    // Clean up the WebSocket connection when the component unmounts
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
+    switch (type) {
+      case NotificationType.Like:
+        return baseStyle + " like";
+      case NotificationType.Comment:
+        return baseStyle + " comment";
+      case NotificationType.Follow:
+        return baseStyle + " follow";
+      default:
+        return baseStyle;
+    }
+  };
 
   return (
     <div className="main-notification-container">
@@ -37,17 +67,40 @@ export default function MainNotification() {
         <h3>Notifications</h3>
       </div>
       <div className="main-notification-body">
-        <div className="main-notification-logo">
-          <img src={Logo} alt="" />
-        </div>
-        <div className="main-notification-content">
-          {/* Map through the notifications and render each one */}
-          {notifications.map((notification, index) => (
-            <div key={index}>
-              <p>{notification.senderId}</p>
-              <p>{notification.message}</p>
-            </div>
-          ))}
+        <div className="main-notification-body-container">
+          {notifications.length > 0 &&
+            notifications.map((notification, index) => (
+              <div
+                key={notification.id}
+                className={getNotificationStyle(
+                  notification.type,
+                  notification.seen
+                )}
+                onClick={() => handleNotificationClick(index)}
+              >
+                <div className="main-notification-avatar">
+                  <div className="main-notification-logo">
+                    <img src={Logo} alt="" />
+                  </div>
+                  <div className="main-notification-content">
+                    <div>
+                      {!notification.seen && (
+                        <>
+                          <p>{`New ${notification.type} Notification`}</p>
+                          {notification.seen && (
+                            <div>
+                              <p>{notification.message}</p>
+                              <p>{moment(notification.createdAt).fromNow()}</p>
+                              {/* Add other additional information as needed */}
+                            </div>
+                          )}
+                        </>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
         </div>
       </div>
     </div>
